b710e0da2a5e6d5d98798e6d11e8e27884f1a328
Aditya <k.aditya00@gmail.com>
2020-04-04 10:26:51 +0530

    test(actions): setup github actions
    - add action.yml
    - add helper function to get commit message

---
+++ src/git-helper.ts
@@ -0,0 +1,25 @@
+import * as exec from "@actions/exec";
+
+export async function getCommitMessage(sha: string): Promise<string> {
+    let message = '';
+
+    const options = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                message += data.toString();
+            }
+        }
+    };
+
+    const args: string[] = [
+        'rev-list',
+        '--format=%B',
+        '--max-count=1',
+        sha
+    ];
+
+    await exec.exec('git', args, options);
+    message.trim();
+
+    return message;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-// Workflow
-// Get the commit somehow
-// Verify if its ok?
+import { getCommitMessage } from "./git-helper";
+
+async function run(): Promise<void> {
+    const commitSHA = github.context.sha;
+    core.debug(`Commit Message SHA:${commitSHA}`);
+
+    const message = await getCommitMessage(commitSHA);
+    core.debug(`Commit Message Found:\n${message}`);
+};
+
+try {
+    run();
+} catch (err) {
+    core.setFailed(`Action failed with error ${err}`);
+}
