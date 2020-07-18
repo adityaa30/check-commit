cc8850e0ebb83ef0eb46ec1efe1f514ab9758a16
Aditya <k.aditya00@gmail.com>
2020-07-19 01:51:19 +0530

    test: add coverage threshold to 80%

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,13 @@ module.exports = {
     transform: {
         '^.+\\.ts$': 'ts-jest'
     },
-    verbose: true
+    verbose: true,
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+        }
+    }
 }
